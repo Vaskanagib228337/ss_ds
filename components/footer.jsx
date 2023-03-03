8cd79abed59b3873b3bd690c56b9725b72704278
Vaskanagib228337 <78267033+Vaskanagib228337@users.noreply.github.com>
2023-03-03 20:00:06 +0500

    first_commit
    1

---
+++ components/footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return(
+        <footer>
+            Все права зашишены &copy;
+        </footer>
+    )
+}
---
+++ components/header.jsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+    return(
+        <header>
+            <div>
+                <span className="logo">ss_ds</span>
+            </div>
+            <div className="presentation"></div>
+
+
+        </header>
+    )
+}
